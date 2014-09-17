3158ffaea86ac83d8154ef31346287c64aa615c0
kaspertorian <kaspertorian@gmail.com>
2014-09-17 11:06:29 +0200

    projekt start.............

---
+++ ProducerConsumer/BoundedBuffer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class BoundedBuffer
+    {
+        Boolean IsFull()
+        {
+         
+            return true;
+        }
+
+        private override void Put(int element);
+
+        private override int Take();
+    }
+}
